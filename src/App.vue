c6bd1f5bc38321aacb0cfe51d8c845999d0ad04f
Mario Mori <mario_mori@hotmail.com>
2024-01-17 23:30:41 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,67 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Welcome to VueJS!!!! <span>&#128512;</span></h1>
+
+  <ul>
+    <li v-for="product in products" >
+      <img :src="product.image" :alt="product.product">
+      <aside>
+          <h2>{{ product.product }} </h2>
+          <h3>{{ product.brand }}</h3>
+          <h3>$ {{ product.price }}</h3>
+          <button @click="editProduct(product.product)">Editar</button>
+      </aside>
+      <div v-if="product.edit"> <ModalEditForm /></div>
+    </li>
+  </ul>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalEditForm from './components/ModalEditForm.vue';
 
 export default {
+
   name: 'App',
+  data() {
+    return {
+      products: [
+        {
+          product:"Cerveza Heineken 473 Ml",
+          brand: "Heineken",
+          image: require("./assets/CervezaHeineken473Ml.png"),
+          price: 845,
+          edit: false,
+        },
+        {
+          product: "Cerveza Corona 330 Ml",
+          brand: "Corona",
+          image: require("./assets/CervezaCoronaNoRetornable330Ml.png"),
+          price: 840,
+          edit: false,
+        },
+        {
+          product: "Cerveza Grolsch 925 Ml",
+          brand : "Grolsch",
+          image : require("./assets/CervezaGrolschRetornable925Ml.png"),
+          price: 1571.25,
+          edit: false,
+        }
+      ],
+    }
+  },
   components: {
-    HelloWorld
+    ModalEditForm
+},
+  methods: {
+    editProduct(product){
+      console.log(product)
+      this.products.forEach((element) => {
+        if (element.product == product) {
+          element.edit = !element.edit
+        }
+      })
+    }
   }
 }
 </script>
@@ -22,5 +74,42 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+    background: #eee;
+    max-width: 980px;
+    margin: 20px auto;
+}
+p, h3, ul {
+    margin: 0;
+    padding: 0;
+}
+ul {
+    display: flex;
+    justify-content: space-between;
+}
+li {
+    list-style-type: none;
+    background: #fff;
+    margin: 20px 20px;
+    padding: 10px 20px;
+    border-radius: 20px;
+    display: flex;
+    /* flex-direction: column; */
+    align-items: center;
+    justify-content: space-between;
+}
+
+li > img {
+    width: 100px;
+}
+
+li > aside {
+    margin: auto 20px;
+}
+
+li.fav {
+    background: #FFFDAD;
 }
 </style>
---
+++ src/components/ModalEditForm.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <div>
+                <label for="product">Nombre: </label>
+                <input type="text" name="product" v-if="name !== undefined" :value="product">
+                <input type="text" name="product" v-else>
+            </div>
+            <div>
+                <label for="brand">Marca: </label>
+                <input type="text" name="brand" v-if="brand !== undefined" :value="brand">
+                <input type="text" name="brand" v-else>
+            </div>
+            <div>
+                <label for="price">Precio: $ </label>
+                <input type="number" name="price" v-if="price !== undefined" :value="price">
+                <input type="number" name="price">
+            </div>        
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        props: [ product, brand, price ]
+    }
+}
+
+</script>
+
+<style scoped>
+
+.backdrop {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: #000;
+
+}
+
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: #fff;
+    border-radius: 10px;
+}
+</style>
